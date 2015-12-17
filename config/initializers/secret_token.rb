d587c6172cfe565a8fc7b537edb2b7dd1b123e26
Cameron Harris <cameroniharris1@gmail.com>
2015-12-17 05:34:06 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Beemindfinance::Application.config.secret_key_base = '2cb6f2c64cf348936e6cff843a866f5bd014bcb2d7c842d622eb59dafce03f6721f07b24bd48ea83bde2b390e20857de71f59bcf60a0e12b19e8fc831ec4e44b'
